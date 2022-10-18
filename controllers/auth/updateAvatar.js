fce09b4d10b0ce67834928b9e39ef0b6ebef1713
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2022-10-18 17:12:07 +0300

    resizeAvatar

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,18 +1,30 @@
 const { User } = require("../../models/user");
 const fs = require("fs/promises");
 const path = require("path");
-
+const Jimp = require("jimp");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   try {
     const { id } = req.user;
     const { path: tempUpload, originalname } = req.file;
+    //
+    const resizeAvatar = await Jimp.read(tempUpload);
+    resizeAvatar
+      .autocrop()
+      .cover(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(tempUpload);
+    //
     const extention = originalname.split(".").pop();
     const filename = `${id}.${extention}`;
-    console.log(filename);
     const resultUpload = path.join(avatarsDir, filename);
+
     await fs.rename(tempUpload, resultUpload);
+
     const avatartURL = path.join("avatars", filename);
     await User.findByIdAndUpdate(id, { avatartURL });
     res.json({
